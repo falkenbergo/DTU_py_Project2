7346918da8f63c5e8c3bc2b3912288654f221c06
SlayerLayer <Falkenbergo@icloud.com>
2023-04-29 20:14:21 +0200

    eh

--- menu/inputNumber.py
+++ menu/inputNumber.py
@@ -7,23 +7,26 @@ Created on Mon Mar 27 16:43:39 2023
 
 
 def inputNumber(prompt):
-# =============================================================================
-#     INPUTNUMBER Prompts user to input a number
-#     
-#     Usage: num = inputNumber(prompt) Displays prompt and asks user
-#     number. Repeats until user inputs a valid number.
-#
-#  - Taken from class lecture
-# =============================================================================
+    # =============================================================================
+    #     INPUTNUMBER Prompts user to input a number
+    #
+    #     Usage: num = inputNumber(prompt) Displays prompt and asks user
+    #     number. Repeats until user inputs a valid number.
+    #
+    #     ErrorHandle: If invalid number of menuItems is inputtet, error message will display.
+    #     Same goes if the value inputtet is not an integer.
+    #
+    #  - Taken from class lecture
+    # =============================================================================
 
     # While loop true until valid number input in try
     # If error then it will pass, and code will try again
     while True:
         try:
-            num = float(input(prompt))
-            break
+            value = int(input(prompt))
+            if 1 <= value <= len(menuItems):
+                return value
+            else:
+                print(f"Invalid choice, please enter a number between 1 and {len(menuItems)}")
         except ValueError:
-            pass
-    
-    # Return float number in varible num
-    return num
+            print("Invalid input, please enter a number")
